5bfc69efad4a1f2a73f46d36a2cedb10e78e5c6f
Rakesh Gangwar <mail@rakeshgangwar.com>
2017-04-14 23:34:56 +0530

    removed apikey

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -5,8 +5,7 @@ class GoogleMap extends Component {
 
     static defaultProps = {
         center: { lat: 59.95, lng: 30.33 },
-        zoom: 11,
-        apiKey: 'AIzaSyBKazAyYLQ1WG6SOW4GpCPvA8D2X-QwQPw'
+        zoom: 11
     };
 
     render() {
