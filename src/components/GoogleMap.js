62d87c6875bbf3e17894c3c1d48a076ecef05497
Rakesh Gangwar <mail@rakeshgangwar.com>
2017-04-17 18:49:13 +0530

    added polyline class (errors exist)

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react'
 import GoogleMapReact from 'google-map-react';
+import Polyline from './Polyline.js'
 
 class GoogleMap extends Component {
+
+    constructor (props, context) {
+        super(props, context)
+        this.state = ({
+            map: null,
+            maps: null,
+            mapLoaded: false
+        })
+    }
+    
 
     static defaultProps = {
         center: { lat: 59.95, lng: 30.33 },
@@ -9,11 +20,21 @@ class GoogleMap extends Component {
     };
 
     render() {
+        function _onClick(obj) { console.log(obj.x, obj.y, obj.lat, obj.lng, obj.event); }
+
         return (
-            <GoogleMapReact
-                defaultCenter={this.props.center}
-                defaultZoom={this.props.zoom}
-            />
+            <div>
+                <GoogleMapReact
+                    onClick={_onClick}
+                    defaultCenter={this.props.center}
+                    defaultZoom={this.props.zoom}
+                    onGoogleApiLoaded={({ map, maps }) => { this.setState({ map: map, maps:maps, mapLoaded: true }) }}
+                    yesIWantToUseGoogleMapApiInternals>
+                </GoogleMapReact>
+                { this.state.mapLoaded && <Polyline map={this.state.map} maps={this.state.maps} /> }
+            </div>
+
+
         )
     }
 }
